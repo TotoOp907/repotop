6cfc4415a7534d0535b92be91a7b117de212d4c4
TotoOp907 <schiro8378@gmail.com>
2025-01-14 10:37:25 +0100

    voici le codebanger

---
+++ codetop.ino
@@ -0,0 +1,18 @@
+// Broche à laquelle la LED est connectée
+const int ledPin = 12;
+
+void setup() {
+  // Configure la broche de la LED comme sortie
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Allume la LED
+  digitalWrite(ledPin, HIGH);
+  // Attend 1 seconde
+  delay(1000);
+  // Éteint la LED
+  digitalWrite(ledPin, LOW);
+  // Attend 1 seconde
+  delay(1000);
+}
